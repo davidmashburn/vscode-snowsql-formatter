8414277c11d277254ac876ab1f59e4346c9ed336
David N. Mashburn <david.n.mashburn@gmail.com>
2024-02-16 13:34:47 -0500

    Add python string mode as well

--- src/extension.js
+++ src/extension.js
@@ -23,9 +23,61 @@ const getConfig = ({ insertSpaces, tabSize }) => ({
 
 const format = (text, config) => sqlFormatter.format(text, config);
 
+const commentPyToSql = (text) => {
+	const lines = text.split('\n');
+	const patterns = [
+		/^\s*#/,        // Match lines starting with any whitespace followed by a #
+		/^;"""/,        // Match lines starting with ;"""
+		/"""\s*--sql$/ // Match lines ending with """--sql
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		for (let pattern of patterns) {
+			if (pattern.test(lines[i])) {
+				lines[i] = '-- ' + lines[i];
+				break;
+			}
+		}
+	}
+
+	return lines.join('\n');
+
+};
+
+const revertSqlToPy = (text) => {
+	const lines = text.split('\n');
+	const patterns = [
+		/^\s-- *#/,        // Match lines starting with any whitespace followed by a #
+		/^-- ;"""/,        // Match lines starting with ;"""
+		/"""\s-- *--sql$/ // Match lines ending with """--sql
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		for (let pattern of patterns) {
+			if (pattern.test(lines[i])) {
+				lines[i] = '-- ' + lines[i];
+				break;
+			}
+		}
+	}
+
+	return lines.join('\n');
+};
+
+const formatPy = (text, config) => {
+	commentedPy = commentPyToSql(text)
+	formattedSql = sqlFormatter.format(commentedPy, config);
+	formattedPy = revertSqlToPy(formattedSql)
+};
+
 module.exports.activate = () =>
 	vscode.languages.registerDocumentRangeFormattingEditProvider('sql', {
 		provideDocumentRangeFormattingEdits: (document, range, options) => [
 			vscode.TextEdit.replace(range, format(document.getText(range), getConfig(options)))
 		]
 	});
+vscode.languages.registerDocumentRangeFormattingEditProvider('py', {
+	provideDocumentRangeFormattingEdits: (document, range, options) => [
+		vscode.TextEdit.replace(range, formatPy(document.getText(range), getConfig(options)))
+	]
+});
